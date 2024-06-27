f59dd8749753d038dbef3461a6f8e1eef69da72b
robin.kron <Robin.krqqn@hotmail.com>
2024-06-27 18:35:31 +0200

    stashmerge

--- src/sequelize/sequelizeClient.ts
+++ src/sequelize/sequelizeClient.ts
@@ -1,23 +1,19 @@
 import { Sequelize, SequelizeOptions } from 'sequelize-typescript';
 import TrackerDevice from './models/trackerDeviceModel.ts';
 import EDGEDevice from './models/edgeDeviceModel.ts';
-import 'dotenv/config'
+import 'dotenv/config';
 import TrackerDeviceData from './models/trackerDeviceDataModel.ts';
 import EDGEDeviceSettings from './models/edgeDeviceSettingsModel.ts';
 import DeviceTrack from './models/deviceTrackModel.ts';
-import { sequelizeConfSchema } from '../types/zod-shemas-and-TS-types.ts';
 
-const { DB_DATABASE, DB_DIALECT, DB_USERNAME, DB_PASSWORD, DB_PORT } =
-  sequelizeConfSchema.parse(process.env);
 
 export async function getSequelizeClient() {
   const sequelizeClient = new Sequelize({
-    database: DB_DATABASE,
-    dialect : DB_DIALECT,
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    port    : DB_PORT,
-    logging : () => null,    //! Implement for production
+    database: process.env.DB_DATABASE,
+    dialect : process.env.DB_DIALECT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port    : process.env.DB_PORT,
     models  : [
       EDGEDevice,
       TrackerDevice,
@@ -27,6 +23,5 @@ export async function getSequelizeClient() {
     ],
   } as SequelizeOptions);
 
-  // ! Remove 'force:' for production
-  return await sequelizeClient.sync({force: true});
+  return await sequelizeClient.sync({ force: true });
 }
